5e3997d3c8b95250d8d66569b09215b358c50c0c
Patrick Liang <pl204511194@yahoo.com>
2021-12-22 14:29:53 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(name, event) {
+    let thankYou=[];
+    for (let i = 0; i<name.length; i++) {
+        thankYou.push("Thank you, "+ name[i] + ", for the wonderful " + event + " gift!");
+    }
+    return thankYou;
+    }
+    writeCards(["Ada", "Brendan", "Ali"], "birthday");
+    
+function countDown(num) {
+        while (num >= 0) {
+            num--;
+            console.log(num+1);
+        }
+}
+    
+    
